0482e098b3b45a72e9f9a7b85787b84129271800
Neel-H-Prajapati <prajapatinimisha853@gmail.com>
2025-03-06 11:38:48 +0530

    first commit

---
+++ client/user/src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="bg-base-200 py-4 text-center">
+      <p>&copy; {new Date().getFullYear()} BookMyGround. All rights reserved.</p>
+      <p>
+        Developed with ❤ by{" "}
+        
+          Neel/Chirag
+      
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
